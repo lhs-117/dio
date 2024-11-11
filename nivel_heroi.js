8ee391fa4b4fd01a860421e7ea884a15180a7d57
Luis <149009255+lhs-117@users.noreply.github.com>
2024-11-11 15:22:50 +0000

    primeiro commit

---
+++ nivel_heroi.js
@@ -0,0 +1,17 @@
+let xp = 999;
+
+if (xp <= 1000) { console.log ('Seu herói é FERRO'); }
+
+else if (xp > 1000 && xp <= 2000) { console.log ('Seu herói é BRONZE'); }
+
+else if (xp > 2000 && xp <= 5000) { console.log ('Seu herói é PRATA'); }
+
+else if (xp > 5000 && xp <= 7000) { console.log ('Seu herói é OURO'); }
+
+else if (xp > 7000 && xp <= 8000) { console.log ('Seu herói é PLATINA'); }
+
+else if (xp > 8000 && xp <= 9000) { console.log ('Seu herói é ASCENDENTE'); }
+
+else if (xp > 9000 && xp <= 10000) { console.log ('Seu herói é IMORTAL'); }
+
+else if (xp > 10000) { console.log ('Seu herói é RADIANTE'); }
